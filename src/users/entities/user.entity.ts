060cda5655bc550d057dc8e32d086fe503ac1b0c
Yievgeniy Shek <shek@chocotravel.com>
2021-08-15 18:07:17 +0600

    fix: role as union for User entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,10 @@
+type Role = 'client' | 'admin' | 'manager';
+
 export class User {
   public _id: string;
   public email: string;
   public passwordHash: string;
   public name: string;
   public contactPhone: string;
-  public role: string;
+  public role: Role;
 }
