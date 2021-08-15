54ed0b753a99a09ae289fe2075b95777f6ee3f89
Yievgeniy Shek <shek@chocotravel.com>
2021-08-15 23:26:59 +0600

    chore: install and connect mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
