a2f535c63e2d10fa628c0b457d78398d574dd98c
Yievgeniy Shek <shek@chocotravel.com>
2021-08-15 17:58:35 +0600

    feat: add User's entity definition

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,8 @@
-export class User {}
+export class User {
+  public _id: string;
+  public email: string;
+  public passwordHash: string;
+  public name: string;
+  public contactPhone: string;
+  public role: string;
+}
