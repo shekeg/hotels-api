c1162f2aa42ca7ec209385e12421773b117032a4
Yievgeniy Shek <shek@chocotravel.com>
2021-08-18 23:24:27 +0600

    chore: init hotels module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { HotelsModule } from './hotels/hotels.module';
 
 @Module({
   imports: [
     UsersModule,
     MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION),
+    HotelsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
