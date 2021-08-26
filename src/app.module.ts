c656209d07ca034a795d1ba5ec3f21badb5cf332
Yievgeniy Shek <shek@chocotravel.com>
2021-08-26 20:43:22 +0600

    chore(reservations): init resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { HotelsModule } from './hotels/hotels.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
   imports: [
     UsersModule,
     MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION),
     HotelsModule,
+    ReservationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
