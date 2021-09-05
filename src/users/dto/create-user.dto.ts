d6d52686e9cd333c72afc29bc9486f777b446f80
Yievgeniy Shek <shek@chocotravel.com>
2021-09-05 18:45:17 +0600

    fix(users): CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,7 @@
 export class CreateUserDto {
   public email: string;
-  public password: string;
+  public passwordHash: string;
   public name: string;
   public contactPhone: string;
+  public role: 'client' | 'admin' | 'manager';
 }
