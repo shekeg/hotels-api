5fdfc459ed55cd4a790ec80e7a8a973eb4832d74
Yievgeniy Shek <shek@chocotravel.com>
2021-09-24 20:39:35 +0600

    chore(hotels) remove redundant controllers

--- src/hotels/hotels.module.ts
+++ src/hotels/hotels.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { HotelsService } from './hotels.service';
-import { HotelsController } from './hotels.controller';
 import { Hotel, HotelSchema } from './hotels.schema';
 import { HotelRoom, HotelRoomSchema } from './hotel-rooms.schema';
-import { HotelRoomsController } from './hotel-rooms.controller';
 import { HotelRoomsService } from './hotel-rooms-service.service';
 
 @Module({
@@ -15,7 +13,6 @@ import { HotelRoomsService } from './hotel-rooms-service.service';
       { name: HotelRoom.name, schema: HotelRoomSchema },
     ]),
   ],
-  controllers: [HotelsController, HotelRoomsController],
   providers: [HotelsService, HotelRoomsService],
   exports: [HotelsService, HotelRoomsService],
 })
