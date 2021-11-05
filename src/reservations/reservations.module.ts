50667c6fd7791e2cabc9a62e8893671d06fec7a1
Yievgeniy Shek <shek@chocotravel.com>
2021-11-05 18:32:18 +0600

    refactor: remove reservations controller since there is api reservation controler exists

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
-import { ReservationsController } from './reservations.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Reservation, ReservationSchema } from './reservations.schema';
 
@@ -10,7 +9,6 @@ import { Reservation, ReservationSchema } from './reservations.schema';
       { name: Reservation.name, schema: ReservationSchema },
     ]),
   ],
-  controllers: [ReservationsController],
   providers: [ReservationsService],
   exports: [ReservationsService],
 })
